af64a5128ab483ea44fef7a2aa0669b65cfdf6d9
robhowley <howley.robert@gmail.com>
2016-02-21 09:44:39 -0500

    first commit boilerplate

---
+++ treetl/__init__.py
@@ -0,0 +1,16 @@
+
+import json
+import pkgutil
+
+from .configuration import ConfigReader
+
+
+# maintain package version
+__pkg_dat = pkgutil.get_data(__package__, 'pkg_info.json').decode('utf-8')
+pkg_info = json.loads(__pkg_dat)
+__version__ = pkg_info['version']
+
+# build package configuration file
+pkg_config = ConfigReader(
+    config_str=pkgutil.get_data(__package__, 'cfg/config.ini')
+)
